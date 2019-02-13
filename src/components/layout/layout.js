94e6d08b72c0967ed4a99b57dcbb2157fa5e4465
Dmitry Vdovichencko <Kenar89@gmail.com>
2019-02-13 09:15:37 +0500

    added layout component

---
+++ src/components/layout/layout.js
@@ -0,0 +1,9 @@
+import React from "react"
+import layoutStyles from "./layout.module.css"
+
+export default ({ children }) => (
+  <div className={layoutStyles.layout}>{children}
+
+    	
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import HeroImage from "../components/hero_image/hero_image"
 import { Link } from "gatsby"
 import Nav from "../components/nav/nav"
 import Header from "../components/header/header"
+import Layout from "../components/layout/layout";
 
 
 export default ()=> {
   // Import result is the URL of your image
   return <div>
+    <Layout>
     <Header>
     <h1>Dmitry Vdovichenko</h1>
     <Nav>
@@ -18,6 +20,7 @@ export default ()=> {
 
   </Header>
 <HeroImage></HeroImage>
+</Layout>
   </div>
 
 }
