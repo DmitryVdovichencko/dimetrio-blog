e6a64a465f2ef586d1c07a933d3d2b3e800f3179
Dmitry Vdovichencko <kenar89@gmail.com>
2019-02-20 17:24:33 +0500

    Creating posts pages

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,37 +1,28 @@
 import React from "react"
 import Header from "../components/header"
-
+import {graphql} from "gatsby"
 
 
 export default ({ data }) => {
-    console.log(data);
+   
     return(
         <div>
             <Header></Header>
-            <div>        
-                <h1>My Site's Files</h1>        
-                    <table>          
-                        <thead>            
-                            <tr>              
-                                <th>relativePath</th>              
-                                <th>prettySize</th>              
-                                <th>extension</th>             
-                                <th>birthTime</th>            
-                            </tr>          
-                        </thead>          
-                        <tbody>            
-                            {data.allFile.edges.map(({ node }, index) => (              
-                                <tr key={index}>                
-                                <td>{node.relativePath}</td>                
-                                <td>{node.prettySize}</td>                
-                                <td>{node.extension}</td>                
-                                <td>{node.birthTime}</td>              
-                                </tr>            
-                                )
-                            )}          
-                        </tbody>        
-                    </table>     
-                 </div>
+            <h2>Aricles</h2>
+            <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+            {data.allMarkdownRemark.edges.map(({ node }) =>(
+                
+                <div key={node.id}>
+                <h3>
+                    {node.frontmatter.title} 
+                
+                </h3>
+                
+                <p>{node.excerpt}</p>
+                </div>
+            )
+            )}
+
         </div>
         
         
@@ -39,16 +30,19 @@ export default ({ data }) => {
 
     }
 export const query = graphql`
-  query {
-    allFile {
+query {
+    allMarkdownRemark {
+      totalCount
       edges {
         node {
-          relativePath
-          prettySize
-          extension
-          birthTime(fromNow: true)
+          id
+          frontmatter {
+            title
+            
+          }
+          excerpt
         }
       }
     }
-}
+  }
 `
