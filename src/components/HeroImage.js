60137942c92aaa1d33c83ff8981c0052d473a0c8
Dmitry Vdovichencko <Kenar89@gmail.com>
2019-05-06 06:46:10 +0500

    add sticker

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -3,12 +3,15 @@ import styles from "./heroImage.module.scss"
 import developer from "../assets/img/developer.svg" 
 import cat from "../assets/img/cat.svg" 
 import flower from "../assets/img/flower.svg" 
+import netlify from "../assets/img/netlify.svg" 
 
 export default () => (
   <div className={styles.image}>
   <div className={styles.dev}>
   <object type="image/svg+xml" className="dev__img" data={developer}>Your browser does not support SVGs</object>
+   <object type="image/svg+xml" className={styles.sticker} data={netlify}>Your browser does not support SVGs</object>
   <svg width="100%" height="200" viewBox="0 0 100% 50%"className="dev__laptop" preserveAspectRatio="xMinYMid meet" >
+   
     <rect x="10" y="10"rx="20" ry="20" width="90%" height="170" />
     <linearGradient id="linear-gradient">
     <stop offset="0%" stopColor="#360033"/>
